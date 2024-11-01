61c6144b98f04d41b862ab92784593f7e6d5e763
MuhammadShahroz <shahroxyt@gmail.com>
2024-11-01 15:14:46 +0500

    Update Global.css

--- governor-house-website-replica/src/app/page.tsx
+++ governor-house-website-replica/src/app/page.tsx
@@ -1,4 +1,4 @@
-import Footer from "./components/Footer"
+
 import Hero from "./components/Hero"
 import Section1 from "./components/Section1"
 import Section2 from "./components/Section2"
@@ -17,7 +17,7 @@ const Home = () => {
   < Section2/>
   < Section3/>
   < Section4/>
-   <Footer/>
+
 
     </>
   )
